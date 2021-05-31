61c280a4e3402b8e1cff19d4d0529f13b3aed315
Jonata Sousa <devjonataj2k@gmail.com>
2021-05-31 13:10:41 -0300

    Teste para herança

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -3,6 +3,7 @@ package aplicacao;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entidades.Jogo;
 import entidades.Tabuleiro;
 
 public class Programa {
@@ -12,6 +13,7 @@ public class Programa {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		Tabuleiro tabuleiro = new Tabuleiro();
+		Jogo jogo = new Jogo();
 
 		tabuleiro.sortearMinas();
 		tabuleiro.checarMinas();
@@ -19,33 +21,33 @@ public class Programa {
 		System.out.println("         CAMPO MINADO");
 		
 		do {
-			tabuleiro.mostrar();
+			jogo.mostrar();
 			System.out.print("Linha: ");
 			int linha = sc.nextInt();
 			System.out.print("Coluna: ");
 			int coluna = sc.nextInt();
 			
-			while (tabuleiro.checarposicao(linha, coluna)) {
+			while (jogo.checarposicao(linha, coluna)) {
 				System.out.println("Posicao informada ja existe tente novamente:");
 				System.out.print("Linha: ");
 				linha = sc.nextInt();
 				System.out.print("Coluna: ");
 				coluna = sc.nextInt();
 			}
-			tabuleiro.mostrarProximas(linha, coluna);
-			vencedor = tabuleiro.mudarPosicao(linha, coluna);
+			jogo.mostrarProximas(linha, coluna);
+			vencedor = jogo.mudarPosicao(linha, coluna);
 			if (!vencedor) {
-				vencedor = tabuleiro.checarVencedor();
+				vencedor = jogo.checarVencedor();
 			}
 			System.out.println();
 			
 		} while (!vencedor);
 		
 		System.out.println();
-		if (tabuleiro.checarVencedor()) {
+		if (jogo.checarVencedor()) {
 			System.out.println("Você achou todas as minas PARABENS !!!");
 		} else {
-			tabuleiro.mostrarMinas();
+			jogo.mostrarMinas();
 			System.out.println();
 			System.out.println("Você explodiu uma mina !!!");
 		}
--- src/entidades/Jogo.java
+++ src/entidades/Jogo.java
@@ -1,6 +1,114 @@
 package entidades;
 
-public class Jogo {
+public class Jogo extends Tabuleiro{
 	
+	private String[][] jogo = new String[][] { { "-", "-", "-", "-", "-", "-", "-", "-" },
+											   { "-", "-", "-", "-", "-", "-", "-", "-" }, 
+											   { "-", "-", "-", "-", "-", "-", "-", "-" },
+											   { "-", "-", "-", "-", "-", "-", "-", "-" }, 
+											   { "-", "-", "-", "-", "-", "-", "-", "-" },
+											   { "-", "-", "-", "-", "-", "-", "-", "-" }, 
+											   { "-", "-", "-", "-", "-", "-", "-", "-" },
+											   { "-", "-", "-", "-", "-", "-", "-", "-" } };
+
+	public void mostrarProximas(int linhas, int colunas) {
+		int linha = 8 - linhas;
+		int coluna = colunas - 1;
+		if (super.tabuleiro[linha][coluna].equals("0")) {
+			if (linha - 1 >= 0) {
+				if (!super.tabuleiro[linha][coluna].equals("*")) {
+					jogo[linha - 1][coluna] = super.tabuleiro[linha][coluna];
+				}
+				if (coluna - 1 >= 0 && !super.tabuleiro[linha][coluna].equals("*")) {
+					jogo[linha - 1][coluna - 1] = super.tabuleiro[linha][coluna];
+				}
+				if (coluna + 1 < 8 && !super.tabuleiro[linha][coluna].equals("*")) {
+					jogo[linha - 1][coluna + 1] = super.tabuleiro[linha][coluna];
+				}
+			}
+			if (coluna - 1 >= 0 && !super.tabuleiro[linha][coluna].equals("*")) {
+				jogo[linha][coluna - 1] = super.tabuleiro[linha][coluna];
+			}
+			if (coluna + 1 < 8 && !super.tabuleiro[linha][coluna].equals("*")) {
+				jogo[linha][coluna + 1] = super.tabuleiro[linha][coluna];
+			}
+			if (linha + 1 < 8) {
+				if (!super.tabuleiro[linha][coluna].equals("*")) {
+					jogo[linha + 1][coluna] = super.tabuleiro[linha][coluna];
+				}
+				if (coluna - 1 >= 0 && !super.tabuleiro[linha][coluna].equals("*")) {
+					jogo[linha + 1][coluna - 1] = super.tabuleiro[linha][coluna];
+				}
+				if (coluna + 1 < 8 && !super.tabuleiro[linha][coluna].equals("*")) {
+					jogo[linha + 1][coluna + 1] = super.tabuleiro[linha][coluna];
+				}
+			}
+		}
+	}
+
+	public void mostrar() {
+
+		System.out.println("linha");
+		for (int i = 0; i < 8; i++) {
+			System.out.print("  " + (8 - i) + "   ");
+			for (int j = 0; j < 8; j++) {
+				System.out.print(" " + jogo[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println();
+		System.out.println("       1 2 3 4 5 6 7 8\n           coluna");
+	}
+
+	public void mostrarMinas() {
+		System.out.println("linha");
+		for (int i = 0; i < 8; i++) {
+			System.out.print("  " + (8 - i) + "   ");
+			for (int j = 0; j < 8; j++) {
+				if (super.tabuleiro[i][j].equals("*")) {
+					jogo[i][j] = super.tabuleiro[i][j];
+					System.out.print(" " + jogo[i][j]);
+				} else {
+					System.out.print(" " + jogo[i][j]);
+				}
+			}
+			System.out.println();
+		}
+		System.out.println();
+		System.out.println("       1 2 3 4 5 6 7 8\n           coluna");
+	}
+	
+	public boolean checarposicao(int linha, int coluna) {
+		if (jogo[8-linha][coluna-1].equals("-")) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	public boolean mudarPosicao(int linha, int coluna) {
+		if (!super.tabuleiro[8-linha][coluna-1].equals("*")) {
+			jogo[8-linha][coluna-1] = super.tabuleiro[8-linha][coluna-1];
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	public boolean checarVencedor() {
+		int cont = 0;
+		for (int i=0; i<8; i++) {
+			for (int j=0; j<8; j++) {
+				if (jogo[i][j].equals("-")) {
+					cont++;
+				}
+			}
+		}
+		if (cont == 8) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 	
 }
